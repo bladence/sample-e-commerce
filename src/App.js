0b1b4ec4af6a6555cd9aa9a4ceff3c0643f0bc7f
Dave Arrazola <dave@bladence.dev>
2020-01-30 14:38:51 -0800

    Added databse authentication for sign ups via Google and Email

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInUpPage from "./pages/sign-in-up/sign-in-up.component";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
     constructor(props) {
@@ -22,9 +22,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            //console.log(this.state.currentUser);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                    console.log(this.state);
+                })
+            } else {
+                this.setState({currentUser: userAuth});
+            }
         });
     }
 
--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import './sign-in-up.styles.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInUpPage = () => (
-    <div className='sign-in-up'><SignIn/></div>
+    <div className='sign-in-up'><SignIn/><SignUp/></div>
 );
 
 export default SignInUpPage;
